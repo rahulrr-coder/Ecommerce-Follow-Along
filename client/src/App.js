382381adea7430756b1915f82af3b8b0b1f9945e
rahulrr-coder <rahulrr5002@protonmail.com>
2025-02-10 16:42:59 +0530

    Complete Milestone 11

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';  // Import directly
+import LoginPage from './pages/LoginPage';  
 import SignUpPage from './pages/SignUpPage';
 import CreateProduct from './pages/CreateProduct';
-
-import "./App.css";
 import Home from './pages/Home';
+import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/"element={<Home/>}/>
         <Route path="/create-product" element={<CreateProduct/>}/>
       </Routes>
     </BrowserRouter>
--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-
 const ErrorHandler = require("./middlewares/Error");
 const cookieParser = require("cookie-parser");
-
 const bodyParser = require("body-parser");
+
+const cors = require("cors")
+app.use(cors())
 app.use(express.json());
 
 app.use(cookieParser());
@@ -14,13 +15,14 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "server/config/.env" });
+  require("dotenv").config({ path: "./config/.env" });
 }
 
 const user = require("./controllers/user");
 const productRoutes = require("./controllers/product")
 app.use("/api/products",productRoutes)
 app.use("/api/v2/user", user);
+app.use("/uploads", express.static("uploads"));
 
 // Middleware for Errors
 app.use(ErrorHandler);
